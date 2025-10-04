5380287429ca1f329e352a9c97b98a7275b294a7
Thiago Freitas <thiagosistemas3@gmail.com>
2025-10-04 18:42:26 -0300

    fix: Update author information and repository URL in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+    requirements = [
+        line.strip() for line in fh if line.strip() and not line.startswith("#")
+    ]
 
 setup(
     name="django-coralogix-otel",
     version="1.0.0",
-    author="Seu Nome",
-    author_email="seu.email@example.com",
+    author="Thiago Freitas",
+    author_email="thiagosistemas3@gmail.com",
     description="Pacote Django para integração simplificada de OpenTelemetry com Coralogix",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seu-usuario/django-coralogix-otel",
+    url="https://github.com/akaytatsu/django-coralogix-otel",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",

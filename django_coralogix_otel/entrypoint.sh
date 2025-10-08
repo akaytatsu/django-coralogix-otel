198a4944cb6e050c712c0dc8efb4d75890cf9f53
Thiago Freitas <thiagosistemas3@gmail.com>
2025-10-08 17:04:28 -0300

    feat: Remove Django Coralogix OpenTelemetry integration files and middleware

--- django_coralogix_otel/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Entrypoint script para Django Coralogix OpenTelemetry
-
-set -e
-
-# Importa e executa o módulo Python
-python -c "import django_coralogix_otel.entrypoint; django_coralogix_otel.entrypoint.main()" "$@"
--- django_coralogix_otel/management/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Commands de management do Django para OpenTelemetry.
-"""
-
-from .check_opentelemetry import Command
-
-__all__ = ["Command"]
